b7ac440c6f6cf10c62b53336626eab1ca8c941a8
zulffaza <zulffaza@gmail.com>
2018-12-09 19:51:38 +0700

    Add java method analysis

---
+++ src/test/java/com/finalproject/automated/refactoring/tool/configuration/AsyncConfig.java
@@ -0,0 +1,32 @@
+package com.finalproject.automated.refactoring.tool.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+/**
+ * @author fazazulfikapp
+ * @version 1.0.0
+ * @since 9 December 2018
+ */
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+
+    private static final String THREAD_NAME_PREFIX = "Java Methods Detection -";
+
+    @Bean
+    public Executor asyncExecutor() {
+        ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
+
+        threadPoolTaskExecutor.setThreadNamePrefix(THREAD_NAME_PREFIX);
+        threadPoolTaskExecutor.setCorePoolSize(Integer.MAX_VALUE);
+        threadPoolTaskExecutor.initialize();
+
+        return threadPoolTaskExecutor;
+    }
+}
